b2ebba209dd58908ff3371b190f525039cf4777e
Nico Vibert <nvibert@vmware.com>
2020-08-19 16:55:37 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "data_center" { default = "SDDC-Datacenter" }
+variable "cluster" { default = "Cluster-1" }
+variable "workload_datastore" { default = "WorkloadDatastore" }
+variable "compute_pool" { default = "Compute-ResourcePool" }
+
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_server" {}
